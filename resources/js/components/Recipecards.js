c57dbaadcf32d32a004533233f5cd24bf4b93178
Shawn Giroux <shawn.giroux90@gmail.com>
2021-03-10 12:55:25 -0500

    Changing responsive grid columns

--- resources/js/components/Recipecards.js
+++ resources/js/components/Recipecards.js
@@ -32,7 +32,7 @@ class RecipeCards extends React.Component {
 
     render() {
         return (
-            <div className="w-full grid lg:grid-cols-2 xl:grid-cols-3 grid-cols-1 md:gap-10 sm:p-8">
+            <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:gap-10 sm:p-8">
                 {this.state.recipes}
             </div>
         );
